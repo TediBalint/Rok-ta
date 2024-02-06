0262521f64117f16261a6becfb9ca79ccdc55a7a
TediBalint <tedi.balint@students.jedlik.eu>
2024-02-06 14:34:40 +0100

    BoosterManager

---
+++ Rokéta/GameObjectModules/ConsoleObjectModules/ConsoleObjectSubclasses/PlayerModules/BoosterManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Rokéta.GameObjectModules.ConsoleObjectModules.ConsoleObjectSubclasses.PlayerModules
+{
+    public static class BoosterManager
+    {
+        public static List<Booster> Boosters = GetBoosters("SaveFiles\\Objects\\Boosters");
+        private static List<Booster> GetBoosters(string parentFolder)
+        {
+            List<Booster> booster = new List<Booster>();
+            if (File.Exists(parentFolder)) Debug.WriteLine($"Error in BoosterManager {parentFolder} Not found"); 
+            else
+            {
+                foreach (string path in Directory.GetFiles(parentFolder))
+                {
+                    booster.Add()
+                }
+            }
+        }
+        
+    }
+}
