215778520ebf04c66bc9dfc0521d66942dd48a17
TediBalint <tedi.balint@students.jedlik.eu>
2024-02-16 23:19:19 +0100

    20%. CLTextState Works.

--- Rokéta/GameObjectModules/UIObjectModules/UIObjectSubclasses/TextUpdateStates/LeftTextStates/CLTextState.cs
+++ Rokéta/GameObjectModules/UIObjectModules/UIObjectSubclasses/TextUpdateStates/LeftTextStates/CLTextState.cs
@@ -1,12 +1,12 @@
-﻿using Rokéta.GameObjectModules.UIObjectModules.UIObjectSubclasses.TextUpdateStates.CenterTextStates;
-
-namespace Rokéta.GameObjectModules.UIObjectModules.UIObjectSubclasses.TextUpdateStates.LeftTextStates
+﻿namespace Rokéta.GameObjectModules.UIObjectModules.UIObjectSubclasses.TextUpdateStates.LeftTextStates
 {
 	public class CLTextState : LeftTextState
 	{
 		protected override int[] getTextPos(int width, int height, int textLength, Padding padding)
 		{
-			return new int[] {padding.Left, height/2};
+			int filledLines = textLength/(width - padding.Left - padding.Right);
+			if (filledLines % 2 == 1) filledLines++;
+			return new int[] {padding.Left, Math.Max(0,height/2-filledLines/2)};
 		}
 	}
 }
